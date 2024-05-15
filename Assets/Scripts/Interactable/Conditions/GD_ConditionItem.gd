873fa69fe409d7ba268273e81bc507ad5cb7cb7e
eVisualUser <sylvain.tosoni.dev@protonmail.com>
2024-05-15 08:59:38 +0200

    Add reversing item condition

--- Assets/Scripts/Interactable/Conditions/GD_ConditionItem.gd
+++ Assets/Scripts/Interactable/Conditions/GD_ConditionItem.gd
@@ -3,6 +3,7 @@ extends InteractableCondition
 class_name ConditionItem
 
 @export var item: ItemConfig
+@export var exceptedToHave: bool = true 
 
 ## Called when a comparaison happend,
 ## you can refuse the interaction to using result.to_false()
@@ -11,7 +12,7 @@ func on_compare(node_a, node_b, result: BoolObject):
     var interactable_b = node_b as Interactable
 
     if interactable_b != null && interactable_a != null:
-        if interactable_a.inventory.have_item(item):
+        if interactable_a.inventory.have_item(item) == exceptedToHave:
             result.to_true()
         else:
             result.to_false()     
